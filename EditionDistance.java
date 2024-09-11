c72ef89310c3db17ead96d7ac588596a16ce0fcb
Erik Suris <87949031+erikvsuris@users.noreply.github.com>
2024-09-11 20:30:07 -0300

    Update EditionDistance.java

--- EditionDistance.java
+++ EditionDistance.java
@@ -1,9 +1,7 @@
-public class EditionDistance {
+public class Main {
     public static int ED(String str1, String str2, int i, int j) {
-
-        if(i == 0 && j == 0) return 0;
-        if(i == 0 || j == 0) return Math.max(i, j);
-
+        if(i == -1) return i + 1;
+        if(j == -1) return j + 1;
         if(str1.charAt(i) == str2.charAt(j)) ED(str1, str2, i-1, j-1);
         int subCost = ED(str1, str2, i-1, j-1) + 1;
         int addCost = ED(str1, str2, i, j-1) + 1;
