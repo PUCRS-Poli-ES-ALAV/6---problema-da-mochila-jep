18b0d34b187b128fdcfdc56d45b13401fee3d084
erikvsuris <erikvsuris@gmail.com>
2024-10-21 00:45:11 -0300

    Finished Fibo

--- Fibo.java
+++ Fibo.java
@@ -1,39 +1,71 @@
 public class Fibo {
-    public static int fiboRec(int n){
-        if(n <= 1){
+    private static int operationsCounter;
+
+    public static int fiboRec(int n) {
+        operationsCounter++;
+        if (n <= 1)
             return n;
-        }
+
         return fiboRec(n-1) + fiboRec(n-2);
     }
 
-    public static int fibo(int n){
+    public static int iterativeFibo(int n) {
         int[] f = new int[n+1];
         f[0] = 0;
         f[1] = 1;
-        for(int i = 2; i <= n; i++){
+
+        for (int i = 2; i <= n; i++) {
+            operationsCounter++;
             f[i] = f[i-1] + f[i-2];
         }
+
         return f[n];
     }
 
-    public static int memoizedFibo(int n){
+    public static int memoizedFibo(int n) {
         int[] f = new int[n+1];
-        for(int i = 0; i <= n; i++){
+
+        for (int i = 0; i <= n; i++) {
+            operationsCounter++;
             f[i] = -1;
         }
+
         return lookupFibo(f, n);
     }
 
-    public static int lookupFibo(int[] f, int n){
-        if(f[n] >= 0){
+    public static int lookupFibo(int[] f, int n) {
+        operationsCounter++;
+        if (f[n] >= 0)
             return f[n];
-        }
-        if(n <= 1){
+
+        if (n <= 1)
             f[n] = n;
-        }else{
+        else
             f[n] = lookupFibo(f, n-1) + lookupFibo(f, n-2);
-        }
+
         return f[n];
     }
-    
+
+    public static int getOperationsCounter() {
+        int aux = operationsCounter;
+        operationsCounter = 0;
+        return aux;
+    }
+
+    // public static int memoizedFibo(int[] f, int n) {
+    //     for (int i = 0; i <= n; i++)
+    //         f[i] = -1;
+
+    //     return lookUpFibo(f, n);
+    // }
+
+    // public static int lookUpFibo(int[] f, int n) {
+    //     if (f[n] >= 0) return f[n];
+
+    //     if (n <= 1) f[n] = n;
+    //     else f[n] = lookUpFibo(f, n - 1) + lookUpFibo(f, n - 2);
+
+    //     System.out.println(f[n]);
+    //     return f[n];
+    // }
 }
--- Main.java
+++ Main.java
@@ -1,24 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("# Fibonacci Recursivo");
-        System.out.println("[4]" + Fibo.fiboRec(4));
-        System.out.println("[8]" + Fibo.fiboRec(8));
-        System.out.println("[16]" + Fibo.fiboRec(16));
-        System.out.println("[32]" + Fibo.fiboRec(32));
+        System.out.println("\f\n\n# Fibonacci Recursivo");
+        System.out.printf("[4] Res: %d, Ops: %d", Fibo.fiboRec(4), Fibo.getOperationsCounter());
+        System.out.printf("\n[8] Res: %d, Ops: %d", Fibo.fiboRec(8), Fibo.getOperationsCounter());
+        System.out.printf("\n[16] Res: %d, Ops: %d", Fibo.fiboRec(16), Fibo.getOperationsCounter());
+        System.out.printf("\n[32] Res: %d, Ops: %d", Fibo.fiboRec(32), Fibo.getOperationsCounter());
 
-        System.out.println("# Fibonacci Iterativo");
-        System.out.println("[4]" + Fibo.fiboRec(4));
-        System.out.println("[8]" + Fibo.fiboRec(8));
-        System.out.println("[16]" + Fibo.fiboRec(16));
-        System.out.println("[32]" + Fibo.fiboRec(32));
+        System.out.println("\n\n# Fibonacci Iterativo");
+        System.out.printf("[4] Res: %d, Ops: %d", Fibo.iterativeFibo(4), Fibo.getOperationsCounter());
+        System.out.printf("\n[8] Res: %d, Ops: %d", Fibo.iterativeFibo(8), Fibo.getOperationsCounter());
+        System.out.printf("\n[16] Res: %d, Ops: %d", Fibo.iterativeFibo(16), Fibo.getOperationsCounter());
+        System.out.printf("\n[32] Res: %d, Ops: %d", Fibo.iterativeFibo(32), Fibo.getOperationsCounter());
+        System.out.printf("\n[128] Res: %d, Ops: %d", Fibo.iterativeFibo(128), Fibo.getOperationsCounter());
+        System.out.printf("\n[1000] Res: %d, Ops: %d", Fibo.iterativeFibo(1000), Fibo.getOperationsCounter());
+        System.out.printf("\n[10000] Res: %d, Ops: %d", Fibo.iterativeFibo(10000), Fibo.getOperationsCounter());
 
-        System.out.println("# Fibonacci Memoizado");
-        System.out.println("[4]" + Fibo.fiboRec(4));
-        System.out.println("[8]" + Fibo.fiboRec(8));
-        System.out.println("[16]" + Fibo.fiboRec(16));
-        System.out.println("[32]" + Fibo.fiboRec(32));
-        System.out.println("[128]" + Fibo.fiboRec(128));
-        System.out.println("[1000]" + Fibo.fiboRec(1000));
-        System.out.println("[10000]" + Fibo.fiboRec(10000));
+        System.out.println("\n\n# Fibonacci Memoizado");
+        System.out.printf("[4] Res: %d, Ops: %d", Fibo.memoizedFibo(4), Fibo.getOperationsCounter());
+        System.out.printf("\n[8] Res: %d, Ops: %d", Fibo.memoizedFibo(8), Fibo.getOperationsCounter());
+        System.out.printf("\n[16] Res: %d, Ops: %d", Fibo.memoizedFibo(16), Fibo.getOperationsCounter());
+        System.out.printf("\n[32] Res: %d, Ops: %d", Fibo.memoizedFibo(32), Fibo.getOperationsCounter());
+        System.out.printf("\n[128] Res: %d, Ops: %d", Fibo.memoizedFibo(128), Fibo.getOperationsCounter());
+        System.out.printf("\n[1000] Res: %d, Ops: %d", Fibo.memoizedFibo(1000), Fibo.getOperationsCounter());
+        System.out.printf("\n[10000] Res: %d, Ops: %d", Fibo.memoizedFibo(10000), Fibo.getOperationsCounter());
     }
 }
